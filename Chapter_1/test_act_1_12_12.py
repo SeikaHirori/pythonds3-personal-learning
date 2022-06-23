4c57ba35fd62ffc7334db9e8757aa0f2848d0da2
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-06-23 13:21:05 -0700

    removed class from pytest;
    Class isn't needed and makes it highly complicated than needed

--- Chapter_1/test_act_1_12_12.py
+++ Chapter_1/test_act_1_12_12.py
@@ -4,10 +4,8 @@ from act_1_12_1 import code
 
 # Activity link: https://runestone.academy/ns/books/published/pythonds3/Introduction/DefiningFunctions.html
 
-class test_code:
-    def __init__(self):
-        self.code_class = code()
+
     
-    def test_hello():
-        testCode = code()
-        assert testCode.get_hello() == 'Hello'
+def test_hello():
+    testCode = code()
+    assert testCode.get_hello() == 'Hello'
