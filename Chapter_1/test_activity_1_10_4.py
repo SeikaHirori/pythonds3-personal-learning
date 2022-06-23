902db318e90d92591c6f3e13d43e3b6f723e0b50
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-06-23 11:44:09 -0700

    current test method functions

--- Chapter_1/test_activity_1_10_4.py
+++ Chapter_1/test_activity_1_10_4.py
@@ -4,22 +4,16 @@ from activity_1_10_4 import comp_list
 
 # Link: https://runestone.academy/ns/books/published/pythonds3/Introduction/ControlStructures.html
 
-# Shortcuts for testing
-
-def captureOutput(capsys):
-    captured = capsys.readouterr()
-    return captured
-
 
 # Testing function from source code
 
 def test_comp_list(capsys):
     comp_list() #function prints at the end
-    # captured = capsys.readouterr()
+    captured = capsys.readouterr()
 
-    # assert captured.out == "['c', 'a', 't', 'd', 'o', 'g', 'r', 'a', 'b', 'b', 'i', 't']\n", "should be ['c', 'a', 't', 'd', 'o', 'g', 'r', 'a', 'b', 'b', 'i', 't']\n"
+    assert captured.out == "['c', 'a', 't', 'd', 'o', 'g', 'r', 'a', 'b', 'b', 'i', 't']\n", "should be ['c', 'a', 't', 'd', 'o', 'g', 'r', 'a', 'b', 'b', 'i', 't']\n"
 
-    assert captureOutput() == "['c', 'a', 't', 'd', 'o', 'g', 'r', 'a', 'b', 'b', 'i', 't']\n", "should be ['c', 'a', 't', 'd', 'o', 'g', 'r', 'a', 'b', 'b', 'i', 't']\n"
+    
 
 # def test_no_duplicate_in_list(capsys)
 
