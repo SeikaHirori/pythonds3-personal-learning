07e493702f6cd628181bc164aa827b08468900ad
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-06-23 10:07:19 -0700

    include act 1.10.1 code for reference

---
+++ Chapter_1/activity_1_10_4.py
@@ -0,0 +1,9 @@
+
+# def act_1_10_1_code(): # code from activity 1.10.1 ; use as reference
+#     word_list = ["cat", "dog", "rabbit"]
+#     letter_list = [ ]
+#     for a_word in word_list:
+#         for a_letter in a_word:
+#             letter_list.append(a_letter)
+#     print(letter_list)
+
