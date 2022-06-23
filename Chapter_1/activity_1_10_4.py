fc0b77e58663e84860f8e9d698c771fb4412d247
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-06-23 12:18:30 -0700

    refactored code

--- Chapter_1/activity_1_10_4.py
+++ Chapter_1/activity_1_10_4.py
@@ -9,38 +9,25 @@
 
 import functools
 
+from collections import OrderedDict
+
 
 def comp_list():
     # the answer is: ['c', 'a', 't', 'd', 'o', 'g', 'r', 'a', 'b', 'b', 'i', 't']
 
     word_list = ["cat", "dog", "rabbit"]
     
-    # Option 1
+    # Option 1 ; my code
     word_string = (''.join(word_list))
     letter_list = [letter for letter in word_string]
 
-    # Option 2
-    letter_list = [word[i] for word in word_list for i in range(len(word))]
+    # # Option 2
+    # letter_list = [word[i] for word in word_list for i in range(len(word))]
+    # print(list(letter_list))
 
-
-    print(list(letter_list))
+    return list(letter_list)
 
     
 def no_duplicate():
-    # the answer is: ['c', 'a', 't', 'd', 'o', 'g', 'r', 'b', 'i']
-
-    word_list = ["cat", "dog", "rabbit"]
     
-    # Option 1
-    # word_string = (''.join(word_list))
-    # letter_list =[letter for letter in word_string]
-
-    # Option 2
-    letter_list = [word[i] for word in word_list for i in range(len(word))]
-
-
-    seen = list(set(letter_list))
-
-
-    print (list(seen))
 
