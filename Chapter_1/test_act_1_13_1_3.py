6b42027fd9af5f7d93c44d6959e2c899a1e9c91e
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-06-24 14:43:01 -0700

    wrote code for tests '__mul__'; '__truediv__'; 'test__sub__'; AND '__lt__';
    '__gt__' needs to be written

--- Chapter_1/test_act_1_13_1_3.py
+++ Chapter_1/test_act_1_13_1_3.py
@@ -1,28 +1,57 @@
 import pytest
 
-from act_1_13_1_3 import 
+from act_1_13_1_3 import Fraction
 
 # URL: https://runestone.academy/ns/books/published/pythonds3/Introduction/ObjectOrientedProgramminginPythonDefiningClasses.html#lst-fractioncode
 
-def test__mul__(): # Multiplication
+def test__mul__(capsys): # Multiplication
     f1 = Fraction(3,5)
+    f2 = Fraction(1,2)
 
-    assert == 
+    f3 = f1 * f2
+    print(f3)
+    captured = capsys.readouterr()
 
-def test__truediv__(): # Division
+    assert captured.out == '3/10\n', 'Should return: 3/10\n'
 
+def test__truediv__(capsys): # Division
+    f1 = Fraction(1,5)
+    f2 = Fraction(2,7)
     
-    assert ==
+    f3 = f1 / f2
+    print(f3)
+    captured = capsys.readouterr()
 
-def test__sub__(): # Subtraction
+    assert captured == '7/10\n', 'Should return: 7/10\n'
 
+def test__sub__(capsys): # Subtraction
+    f1 = Fraction(2, 5)
+    f2 = Fraction(2, 6)
 
-    assert == 
+    f3 = f1 - f2
+        # '24/30' before simplifying
+        # '4/5' afterwards
+    print(f3)
+    captured = capsys.readouterr()
+
+    assert captured == '4/5\n', 'Should return: 4/5\n'
 
 def test__lt__(): # Less Than | a < b
+    # False result
+    f1 = Fraction(3,10)
+    f2 = Fraction(1,10)
 
+    f3 = f1 < f2
 
-    assert ==
+    assert f3 == False, 'Should return: False'
+
+    # True result
+    w1 = Fraction(1,19)
+    w2 = Fraction(1,2)
+
+    w3 = w1 < w2
+
+    assert w3 == True,' Should return: True'
 
 def test__gt__(): # Greater than | a > b
 
