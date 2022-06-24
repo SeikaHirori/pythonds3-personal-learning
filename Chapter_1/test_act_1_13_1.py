55273d507101d17cc80fd029e120f73b145f3b99
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-06-24 06:12:31 -0700

    wrote test 'test_show'

---
+++ Chapter_1/test_act_1_13_1.py
@@ -0,0 +1,11 @@
+from act_1_13_1 import Fraction
+
+
+
+def test_show(capsys):
+    testFrac = Fraction(3,5)
+
+    testFrac.show()
+    captured = capsys.readouterr()
+
+    assert captured == "3/5\n"
