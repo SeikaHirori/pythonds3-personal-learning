dec58352922d58169309511b7eb8e134e372d2f1
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-06-24 14:12:54 -0700

    created files for Activity 1.13.1.3
    URL: https://runestone.academy/ns/books/published/pythonds3/Introduction/ObjectOrientedProgramminginPythonDefiningClasses.html#lst-fractioncode

---
+++ Chapter_1/act_1_13_1_3.py
@@ -0,0 +1,4 @@
+
+
+# URL Link: https://runestone.academy/ns/books/published/pythonds3/Introduction/ObjectOrientedProgramminginPythonDefiningClasses.html
+
---
+++ Chapter_1/test_act_1_13_1_3.py
@@ -0,0 +1,30 @@
+import pytest
+
+from act_1_13_1_3 import 
+
+# URL: https://runestone.academy/ns/books/published/pythonds3/Introduction/ObjectOrientedProgramminginPythonDefiningClasses.html#lst-fractioncode
+
+def test__mul__(): # Multiplication
+
+
+    assert == 
+
+def test__truediv__(): # Division
+
+    
+    assert ==
+
+def test__sub__(): # Subtraction
+
+
+    assert == 
+
+def test__lt__(): # Less Than | a < b
+
+
+    assert ==
+
+def test__gt__(): # Greater than | a > b
+
+
+    assert ==
