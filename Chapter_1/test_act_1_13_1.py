47ae5ba7605415b2619cba5cce934314470c0b92
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-06-24 09:27:23 -0700

    created test ' test__str__(capsys)'

--- Chapter_1/test_act_1_13_1.py
+++ Chapter_1/test_act_1_13_1.py
@@ -1,6 +1,6 @@
 from act_1_13_1 import Fraction
 
-
+# Link to URL: https://runestone.academy/ns/books/published/pythonds3/Introduction/ObjectOrientedProgramminginPythonDefiningClasses.html #a-fraction-class
 
 def test_show(capsys):
     testFrac = Fraction(3,5)
@@ -9,3 +9,11 @@ def test_show(capsys):
     captured = capsys.readouterr()
 
     assert captured.out == "3/5\n"
+
+def test__str___(capsys):
+    testFrac = Fraction(1,6)
+
+    str(testFrac)
+    captured = capsys.readouterr()
+
+    assert captured.out == "1/6\n"
