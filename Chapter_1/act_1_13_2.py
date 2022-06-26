4368ab2fa394421b972a842f03e0db63b29d707a
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-06-26 08:16:23 -0700

    writing test code for 'test_AndGate'

--- Chapter_1/act_1_13_2.py
+++ Chapter_1/act_1_13_2.py
@@ -1,7 +1,8 @@
 
 
+# Lesson URL: https://runestone.academy/ns/books/published/pythonds3/Introduction/ObjectOrientedProgramminginPythonDefiningClasses.html?lastPosition=13421
 
-class LogicGate:
+class LogicGate: 
     def __init__(self, lbl):
         self.label = lbl
         self.output = None
@@ -12,3 +13,49 @@ class LogicGate:
     def get_output(self):
         self.output = self.perform_gate_logic()
         return self.output
+
+
+class BinaryGate(LogicGate):
+    
+    def __init__(self, lbl):
+        super().__init__(lbl)
+        self.pin_a = None
+        self.pin_b = None
+    
+    def get_pin_a(self):
+        
+        return int(input(f'Enter pin A input for gate {self.get_label()}: '))
+    
+    def get_pin_b(self):
+        
+        return int(input(f'Enter pin B input for gate {self.get_label()}: '))
+
+class UnaryGate(LogicGate):
+    def __init__(self, lbl):
+        super().__init__(lbl)
+        self.pin = None
+    
+    def get_pin(self):
+        return int(input(f'Enter pin input for gate {self.get_label}: '))
+
+class AndGate(BinaryGate):
+    def __init__(self, lbl):
+        super().__init__(lbl)
+    
+    def perform_gate_logic(self):
+        a = self.get_pin_a()
+        b = self.get_pin_b()
+
+        if a == 1 and b == 1:
+            return 1
+        else:
+            return 0
+
+
+
+if __name__ == "__main__":
+    g1 = AndGate("G1")
+    print(g1.get_output())
+
+
+    
--- Chapter_1/test_act_1_13_2.py
+++ Chapter_1/test_act_1_13_2.py
@@ -1,1 +1,21 @@
 import pytest
+import io
+
+from act_1_13_2 import AndGate, LogicGate, BinaryGate, UnaryGate
+
+def test_AndGate(monkeypatch, capsys):
+    g1 = AndGate('G1')
+
+
+    # Non-working code; to be removed after getting a working code
+        # monkeypatch.setattr('sys.stdin', io.StringIO('1\n'))
+
+        # monkeypatch.setattr('sys.stdin', io.StringIO('0'))
+
+    output = g1.perform_gate_logic()
+
+    # captured = capsys.readouterr()
+
+    assert output == 0, "Should return: 0"
+
+
