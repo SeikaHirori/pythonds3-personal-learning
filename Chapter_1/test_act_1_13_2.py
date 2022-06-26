b2dd595320f461b5c319d74cf39f0491b2e4596e
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-06-26 11:05:25 -0700

    create note file for resources used

--- Chapter_1/test_act_1_13_2.py
+++ Chapter_1/test_act_1_13_2.py
@@ -3,22 +3,32 @@ import io
 
 from act_1_13_2 import AndGate, LogicGate, BinaryGate, UnaryGate
 
-def test_AndGate(monkeypatch, capsys):
+def test_AndGate_mocker_original(monkeypatch, capsys):
     g1 = AndGate('G1')
 
     # Try to use mock WITH and WITHOUT pytest-mock:
         # SFO: https://stackoverflow.com/a/56498519
-    # Method #1: Using 'the_prompt'
+    # Method #1: Using stock mocker
     def test_inputs(the_prompt):
         prompt_to_return_val = {'Enter pin A input for gate G1: ': '1', 'Enter pin B input for gate G1: ': '0'}
         val = prompt_to_return_val[the_prompt]
         return val
 
     monkeypatch.setattr('builtins.input', test_inputs)
+    # monkeypatch.setattr('sys.stdin', test_inputs)
     output = g1.perform_gate_logic()
 
     # captured = capsys.readouterr()
 
     assert output == 0, "Should return: 0"
+
+def test_AndGate_PyTest_Mocker(mocker):
+    # Method #2 for Mocker; Using Pytest-mocker
+    # Use this toolset: https://github.com/pytest-dev/pytest-mock
+
+
+    g1 = AndGate('G1')
+
+    mocker.patch('sys.stdin')
 
 
---
+++ Notes/Notes.md
@@ -0,0 +1,7 @@
+
+- buiitins.input VS sys.stdin:
+    - https://stackoverflow.com/questions/29527023/what-is-the-difference-between-input-and-sys-stdin
+
+- Mocker | using builtin AND Pytest-mocker
+    - https://stackoverflow.com/questions/56498487/how-can-i-test-a-loop-with-multiple-input-calls/56498519#56498519
+
