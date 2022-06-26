409d7f8c1b7ab4e35fa44c2efefc890a6958cb59
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-06-26 09:02:52 -0700

    regular mocker failed

--- Chapter_1/test_act_1_13_2.py
+++ Chapter_1/test_act_1_13_2.py
@@ -8,12 +8,19 @@ def test_AndGate(monkeypatch, capsys):
 
     # Try to use mock WITH and WITHOUT pytest-mock:
         # SFO: https://stackoverflow.com/a/56498519
+    # Method #1: Using 'the_prompt'
+    def test_inputs(the_prompt):
+        prompt_to_return_val = {'Enter pin A input for gate G1: ': '1', 'Enter pin B input for gate G1: ': '0'}
+        val = prompt_to_return_val[the_prompt]
+        return val
+
 
     # Non-working code; to be removed after getting a working code
         # monkeypatch.setattr('sys.stdin', io.StringIO('1\n'))
 
         # monkeypatch.setattr('sys.stdin', io.StringIO('0'))
 
+    monkeypatch.setattr('builtins.stdin', test_inputs())
     output = g1.perform_gate_logic()
 
     # captured = capsys.readouterr()
