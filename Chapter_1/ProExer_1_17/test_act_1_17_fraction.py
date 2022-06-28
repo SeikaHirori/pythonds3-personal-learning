282f26d7566c6f94b99cd7e0ea185e3f4103d430
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-06-28 11:38:58 -0700

    wrote test for #6, 'test_negative_denominator()'

--- Chapter_1/ProExer_1_17/test_act_1_17_fraction.py
+++ Chapter_1/ProExer_1_17/test_act_1_17_fraction.py
@@ -165,15 +165,15 @@ def test_constructor_check_den_valid():
     # with pytest.raises(Exception) as e_info: # Now checking for 
     #     f1 = Fraction(2, "meeeeeep")
 
-
-    
     f2 = Fraction(1,2)
 
     assert f2.getNum() == 1, "Should return Num: 1"
     assert f2.getDen() == 2, "Should return Den: 2"
 
 
-    
+#6
+def test_negative_denominator():
+    pass
 
     
 
