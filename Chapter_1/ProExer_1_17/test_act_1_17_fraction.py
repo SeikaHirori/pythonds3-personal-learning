49c6d50ce38dc2ede33d93852de7442d92b9099b
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-06-28 10:39:43 -0700

    wrote test names 'test_GCD_with_mul' && 'test_GCD_with_trueDiv'

--- Chapter_1/ProExer_1_17/test_act_1_17_fraction.py
+++ Chapter_1/ProExer_1_17/test_act_1_17_fraction.py
@@ -47,7 +47,6 @@ def test_GCD_is_used_immediately():
 
     assert f1.getDen() == 2
 
-# TODO
 #2
 def test_GCD_with_addition():
 
@@ -60,7 +59,6 @@ def test_GCD_with_addition():
     assert output_gcd_a.getDen() == 2, "Should return Den: 2"
 
 
-#TODO
 #2
 def test_GCD_with_subtraction():
     # pass
@@ -73,6 +71,15 @@ def test_GCD_with_subtraction():
     assert output_gcd_sub.getNum() == 1, "Should return numerator: 1"
     assert output_gcd_sub.getDen() == 2, "Should return Den: 2"
 
+#TODO
+#2
+def test_GCD_with_mul():
+    pass
+
+#TODO
+#2
+def test_GCD_with_trueDiv():
+    pass
 
 #4
 def test__ge__():
