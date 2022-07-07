ad082a4f3388514c65867c9abb52f822d379c5e9
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-07-07 14:19:11 -0700

    wrote assert for XNOR

--- Chapter_1/ProExer_1_17/test_act_1_17_Gates.py
+++ Chapter_1/ProExer_1_17/test_act_1_17_Gates.py
@@ -25,8 +25,18 @@ def test__XOR__(): # TODO
     assert output == "write later"
 
 #p10
-def test__XNOR__(): #TODO
+def test__XNOR__return_0(): #TODO
 
+    output_0_and_0 = None
+    assert output_0_and_0 == 0, "0 and 0 should result in 0"
+
+
+    output_1_and_1 = None
+    assert output_1_and_1 == 0, "1 and 1 should result in 0"
+
+
+#p10
+def test__XNOR__return_1():
+    
     output = None
-
-    assert output == "write later"
+    assert output == 1, "If both 0 and 1 are in either A and B, it should result in 1"
