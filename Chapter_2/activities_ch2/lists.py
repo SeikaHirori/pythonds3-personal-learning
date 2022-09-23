ea9ccfa0a2e8ccc23cf892d49a5d9a167efd6815
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-09-23 11:40:17 -0700

    Wrote various versions of lists

--- Chapter_2/activities_ch2/lists.py
+++ Chapter_2/activities_ch2/lists.py
@@ -2,3 +2,19 @@
     Link: https://runestone.academy/ns/books/published/pythonds3/AlgorithmAnalysis/Lists.html
 """
 
+def test1():
+    listo:list = []
+    for i in range(1000):
+        listo = listo + 1
+
+def test2():
+    listo:list = []
+    for i in range(1000):
+        listo.append(i)
+
+def test3():
+    l = [i for i in range(1000)]
+
+def test4():
+    l = list(range(1000))
+
