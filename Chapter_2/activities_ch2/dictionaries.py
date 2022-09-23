04c55a3a373aead1016de6667d4b7c45fa777f90
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-09-23 13:05:57 -0700

    Finished Section 2.7: Dictionaries

--- Chapter_2/activities_ch2/dictionaries.py
+++ Chapter_2/activities_ch2/dictionaries.py
@@ -5,3 +5,15 @@
 from timeit import Timer
 import random
 
+# Listings 6
+print(f"{'n':10s}{'list':>10s}{'dict':>10s}")
+for i in range(10_000, 1_000_001, 20_000):
+    t = Timer(f"random.randrange({i}) in x", "from __main__ import random, x")
+    
+    x = list(range(i))
+    list_time = t.timeit(number=1000)
+
+    x = {j: None for j in range(i)}
+    dict_time = t.timeit(number=1000)
+
+    print(f"{i:<10} {list_time:>10.3f} {dict_time:>10.3f} ")
