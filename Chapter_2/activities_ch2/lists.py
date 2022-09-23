6d2141cc5f87365a98fdf755bff50a8867799360
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-09-23 11:57:57 -0700

    Wrote timeit to test how long functions run

--- Chapter_2/activities_ch2/lists.py
+++ Chapter_2/activities_ch2/lists.py
@@ -2,10 +2,12 @@
     Link: https://runestone.academy/ns/books/published/pythonds3/AlgorithmAnalysis/Lists.html
 """
 
+from timeit import Timer
+
 def test1():
     listo:list = []
     for i in range(1000):
-        listo = listo + 1
+        listo = listo + [i]
 
 def test2():
     listo:list = []
@@ -18,3 +20,14 @@ def test3():
 def test4():
     l = list(range(1000))
 
+t1 = Timer("test1()", "from __main__ import test1")
+print(f"concatenation: {t1.timeit(number=1000):15.2f} milliseconds ")
+
+t2 = Timer("test2()", "from __main__ import test2")
+print(f"appending: {t2.timeit(number=1000):19.2f} milliseconds")
+
+t3 = Timer("test3()", "from __main__ import test3")
+print(f"list comprehension: {t3.timeit(number=1000): 10.2f} milliseconds")
+
+t4 = Timer("test4()", "from __main__ import test4")
+print(f"list range: {t4.timeit(number=1000):18.2f} milliseconds")
