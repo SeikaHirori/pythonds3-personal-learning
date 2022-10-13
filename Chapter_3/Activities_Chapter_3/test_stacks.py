e3c425709c9841c2df98dd2f1d283bb0543b3a27
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-10-13 14:10:31 -0700

    Finished writing tests for Stack

--- Chapter_3/Activities_Chapter_3/test_stacks.py
+++ Chapter_3/Activities_Chapter_3/test_stacks.py
@@ -6,9 +6,11 @@ def test_Stack():
     - Add 3 items to list check if: 
         - list is NOT empty
         - size of list is 3
+        - peek at top value
     - Pop two items and check if:
         - correct items were grabbed from top of stack
         - list size is 1
+        - peek at top value
     - Pop last item and check if:
             - last item is correct
             - list is FINALLY empty
@@ -17,14 +19,18 @@ def test_Stack():
     stack_class = Stack()
     assert stack_class.is_empty() == True
 
-    new_items:list[int] = [1, 2, 3]
+    new_items:list[int] = ["hello", "world", ":3"]
     for item in new_items:
         stack_class.push(item)
     assert stack_class.is_empty() == False
     assert stack_class.size() == 3
-    assert stack_class.peek() == 
+    assert stack_class.peek() == ":3"
 
     item_3, item_2 = stack_class.pop(), stack_class.pop()
+    assert item_3 == new_items[2]
+    assert item_2 == new_items[1]
+    assert stack_class.peek() == new_items[0]
 
-    assert item_3 == 3
-    assert item_2 == 2
+    item_1 = stack_class.pop()
+    assert item_1 == new_items[0]
+    assert stack_class.is_empty() == True
