e893e6daf9089a20d57cc48dcb4bf49123857ca4
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-11-02 13:58:56 -0700

    Wrote class "Printer"

--- Chapter_3/Activities_Chapter_3/printing_tasks.py
+++ Chapter_3/Activities_Chapter_3/printing_tasks.py
@@ -3,12 +3,28 @@ Link: https://runestone.academy/ns/books/published/pythonds3/BasicDS/SimulationP
 """
 
 class Printer:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, ppm) -> None:
+        self.page_rate = ppm
+        self.current_task = None
+        self.time_remaining = 0
+    
+    def tick(self):
+        if self.current_task is not None:
+            self.time_remaining = self.time_remaining - 1
+            if self.time_remaining <= 0:
+                self.current_task = None
+    
+    def busy(self):
+        return self.current_task is not None
+    
+    def start_next(self, new_task):
+        self.current_task = new_task
+        self.time_remaining = new_task.get_pages() * 60 / self.page_rate
 
 class Task:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, time) -> None:
+        self.timestamp = time
+
 
 class PrintQueue:
     def __init__(self) -> None:
