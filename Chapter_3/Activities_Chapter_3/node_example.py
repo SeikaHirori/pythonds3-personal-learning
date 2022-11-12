99817969094e6151ec8d036f9080d1e7c03c6dd4
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-11-11 20:05:19 -0800

    setup modules for Node; need to fill rest of functions and data

--- Chapter_3/Activities_Chapter_3/node_example.py
+++ Chapter_3/Activities_Chapter_3/node_example.py
@@ -1,8 +1,9 @@
-
+""" 3.21.1. The Node Class
+    Link: https://runestone.academy/ns/books/published/pythonds3/BasicDS/ImplementinganUnorderedListLinkedLists.html#the-node-class
+"""
 
 class Node:
 
     def __init__(self, node_data) -> None:
         self._data = node_data
         self._next = None
-        
--- Chapter_3/Activities_Chapter_3/test_node_example.py
+++ Chapter_3/Activities_Chapter_3/test_node_example.py
@@ -1,3 +1,7 @@
+""" 3.21.1. The Node Class
+    Link: https://runestone.academy/ns/books/published/pythonds3/BasicDS/ImplementinganUnorderedListLinkedLists.html#the-node-class
+"""
+
 from node_example import Node
 
 def test_1():
