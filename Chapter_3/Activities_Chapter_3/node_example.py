d9ea88866bfdab49e671220e10820b81a47a7bf5
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-11-11 20:24:17 -0800

    Finished experimenting with Node example

--- Chapter_3/Activities_Chapter_3/node_example.py
+++ Chapter_3/Activities_Chapter_3/node_example.py
@@ -7,3 +7,24 @@ class Node:
     def __init__(self, node_data) -> None:
         self._data = node_data
         self._next = None
+    
+    def get_data(self):
+        """Get node data"""
+        return self._data
+    
+    def set_data(self, node_data):
+        """Set node data"""
+        self._data = node_data
+    
+    data = property(get_data, set_data)
+
+    def get_next(self):
+        return self._next
+    
+    def set_next(self, node_next):
+        self._next = node_next
+    
+    next = property(get_next, set_next)
+
+    def __str__(self) -> str:
+        return str(self._data)
--- Chapter_3/Activities_Chapter_3/test_node_example.py
+++ Chapter_3/Activities_Chapter_3/test_node_example.py
@@ -4,7 +4,18 @@
 
 from node_example import Node
 
-def test_1():
+def test_data():
     n = Node(93)
 
     assert n.data == 93
+
+    n.set_data(10)
+    assert n.data == 10
+
+def test_next():
+    foo:Node = Node(93)
+    assert foo.next == None
+
+    bar:Node = Node(20)
+    foo.set_next(bar.get_data())
+    assert foo.next == 20
