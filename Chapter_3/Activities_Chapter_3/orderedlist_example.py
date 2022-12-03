a2c8d0d685c5a677a6fa331168110ab0687d6276
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-12-03 12:56:23 -0800

    Setup: 3.23. Implementing an Ordered List

---
+++ Chapter_3/Activities_Chapter_3/orderedlist_example.py
@@ -0,0 +1,31 @@
+""" 3.23. Implementing an Ordered List¶
+    - Link: https://runestone.academy/ns/books/published/pythonds3/BasicDS/ImplementinganOrderedList.html
+"""
+from node_example import Node
+
+class OrderList:
+    def __init__(self) -> None: # TODO
+        self._head = None
+        self._tail = None
+        self._count = None
+    
+    def add(self, item) -> None: # TODO
+        pass
+
+    def remove(self, item) -> None: # TODO
+        pass
+
+    def search(self, item) -> bool: # TODO
+        pass
+
+    def is_empty(self) -> bool: # TODO
+        pass
+
+    def size(self) -> int: # TODO
+        pass
+
+    def index(self, item) -> int: # TODO
+        pass
+
+    def pop(self, pos=-1) -> Node: # TODO
+        pass
---
+++ Chapter_3/Activities_Chapter_3/test_orderedlist_example.py
@@ -0,0 +1,36 @@
+""" 3.23. Implementing an Ordered List¶
+    - Link: https://runestone.academy/ns/books/published/pythonds3/BasicDS/ImplementinganOrderedList.html
+"""
+import pytest
+from orderedlist_example import OrderList as odl
+
+def finish_your_test():
+    assert 'finish your test' == 'ガンベリ！'
+
+def test_add():
+    o:odl = odl()
+    finish_your_test()
+
+def test_remove():
+    o:odl = odl()
+    finish_your_test()
+
+def test_search():
+    o:odl = odl()
+    finish_your_test()
+
+def test_is_empty():
+    o:odl = odl()
+    finish_your_test()
+
+def test_size():
+    o:odl = odl()
+    finish_your_test()
+
+def test_index():
+    o:odl = odl()
+    finish_your_test()
+
+def test_pop():
+    o:odl = odl()
+    finish_your_test()
