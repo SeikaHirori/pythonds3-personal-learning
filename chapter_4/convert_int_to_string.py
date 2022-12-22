9a1f8a98b0c3c710a32bb043b16efe25c43b6862
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-12-22 13:59:13 -0800

    Finished to_str()

--- chapter_4/convert_int_to_string.py
+++ chapter_4/convert_int_to_string.py
@@ -1,3 +1,8 @@
 
-def to_str(n, base):
-    pass
+def to_str(n, base) -> str:
+    convert_string = "0123456789ABCDEF"
+    
+    if n < base:
+        return convert_string[n]
+    
+    return to_str(n // base, base) + convert_string[n % base]
--- chapter_4/test_convert_int_to_string.py
+++ chapter_4/test_convert_int_to_string.py
@@ -1,5 +1,9 @@
 from convert_int_to_string import *
 
 def test_to_str():
+    output = to_str(n=769, base=10)
+    assert output == '769'
+    
     output = to_str(n=1329, base=16)
-    assert output == 531
+    assert output == '531'
+
