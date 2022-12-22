5869fb74e22b4ce154dc4870e455673ad43f50de
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-12-22 14:23:37 -0800

    Second style also works

--- chapter_4/recursion_sc_2.py
+++ chapter_4/recursion_sc_2.py
@@ -11,8 +11,14 @@ def is_pal(s:str) -> bool:
 def remove_white(s:str) -> str:
     return ''.join(char for char in s if char.isalpha()) # RFER 5
 
+# def reverse_string(s:str) -> str: # Starting from back
+#     if len(s) == 1:
+#         return s[0]
+    
+#     return s[-1]+ reverse_string(s[:len(s) - 1])
+
 def reverse_string(s:str) -> str:
     if len(s) == 1:
         return s[0]
     
-    return s[-1]+ reverse_string(s[:len(s) - 1])
+    return reverse_string(s[1:]) + s[0]
