da236e1cd3629333b1d82ec93b9550780a1b29b1
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-12-23 13:33:49 -0800

    Finished lst_turt1.py and starting: Activity: 4.7.1 Drawing a Recursive Spiral using ``turtle`` (lst_turt1)

--- chapter_4/lst_turt1.py
+++ chapter_4/lst_turt1.py
@@ -1,4 +1,13 @@
 import turtle
 
 def draw_spiral(my_turtle:turtle, line_len:int):
-    pass
+    if line_len > 0:
+        my_turtle.forward(line_len)
+        my_turtle.right(90)
+        draw_spiral(my_turtle, line_len - 5)
+
+if __name__ == '__main__':
+    my_turtle:turtle.Turtle = turtle.Turtle()
+    my_win = turtle.Screen()
+    draw_spiral(my_turtle, 100)
+    my_win.exitonclick()
