315106f0a3ea012d3d4ee9f4d0edcb8ed151525c
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-12-23 13:15:13 -0800

    Finished lst_recstack

--- chapter_4/lst_recstack.py
+++ chapter_4/lst_recstack.py
@@ -1,4 +1,18 @@
 from pythonds3.basic import Stack
 
-def to_str():
-    pass
+def to_str(n, base):
+    the_stack:Stack = Stack()
+    convert_string = "0123456789ABCDEF"
+
+    while n > 0:
+        if n < base:
+            the_stack.push(convert_string[n])
+        else:
+            the_stack.push(convert_string[n % base])
+        n = n // base
+
+    output = ''
+    while not the_stack.is_empty():
+        output = output + str(the_stack.pop())
+
+    return output
